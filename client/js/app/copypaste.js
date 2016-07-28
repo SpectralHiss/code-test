af730274332b577a5479a153ad6d6424cdc4ca71
Houssem El Fekih <hassoum92@hotmail.com>
2016-07-28 15:45:24 +0100

    forgot paste in commit .. oops!

--- client/js/app/copypaste.js
+++ client/js/app/copypaste.js
@@ -3,6 +3,7 @@ define(['jquery', 'app/reporter'], function($, reporter){
 	return {
 		registerHooks: function(){
 			$('form input').each(function(_, elem) {
+				 	
 				 	$(elem).on('copy',function(ev){
   						eventReport = {
   						"eventType": "copyAndPaste",
@@ -11,9 +12,22 @@ define(['jquery', 'app/reporter'], function($, reporter){
   						"pasted": false, // from what is suggested in doc we assume this means copy event.
   						"formId": $(ev.target).attr('id') };
 
-  					reporter.postData(eventReport);
+  						reporter.postData(eventReport);
 
-				 });
+				 	});
+
+				 	$(elem).on('paste',function(ev){
+				 		eventReport = {
+  						"eventType": "copyAndPaste",
+  						"websiteUrl": window.location.href,
+  						"sessionId": "123123-123123-123123123",
+  						"pasted": true, // from what is suggested in doc we assume this means copy event.
+  						"formId": $(ev.target).attr('id') };
+
+  						reporter.postData(eventReport);
+
+				 	});
+
 			});
 		}
 	}
